241a8056ef3455edd2d238c9c2bf042265a25f8e
Joseph Safari <sajo2460hotmail.com>
2019-11-20 16:31:08 +0000

    Add css and jss tasks to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
 const {src, dest} = require('gulp');
+const origin = 'src';
+const destination = 'build';
 
-function defaultTask(cb) {
+//defaultTask as html
+function html(cb) {
     // place code for your default task here
-    src('src/index.html').pipe(dest('build'));
-    cb();
-  }
+    /* src('src/index.html').pipe(dest('build'));
+     */
+  src(`${origin}**/*.html`).pipe(dest(destination))
+  cb();
+}
+function css(cb) {
+  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`))
+  cb();
+}
+function js(cb) {
+  src([
+    `${origin}/js/lib/bootstrap.bundle.min.js`,
+    `${origin}/js/lib/fontawesome-all.min.js`,
+    `${origin}/js/lib/jquery.min.js`,
+    `${origin}/js/script.js`
+  ]).pipe(dest(`${destination}/js`))
+  cb();
+}
   
-  exports.default = defaultTask
+  exports.html = html;
+  exports.css = css;
+  
+  exports.html = html;
+  exports.css = css;
+  exports.js = js;
