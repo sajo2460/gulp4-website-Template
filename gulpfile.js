465d66416bb0d4add49c2c2788c694c32b7c0e26
Joseph Safari <sajo2460hotmail.com>
2019-11-20 23:47:37 +0000

    Introduce sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const {src, dest, series, parallel, watch} = require('gulp');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 const babel= require('gulp-babel');
-const concatenate = require('gulp-concat');
 
 const origin = 'src';
 const destination = 'build';
+
+sass.compiler = require('node-sass');
 
 async function clean(cb) {
   await del(destination);
@@ -19,25 +21,32 @@ function html(cb) {
 }
 
 function css(cb) {
-  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`));
+  src([`${origin}/css/animate.css`]).pipe(dest(`${destination}/css`));
+
+  src(`${origin}/css/style.scss`)
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  
+  .pipe(dest(`${destination}/css`));
+
   cb();
 }
 
 function js(cb) {
   src(`${origin}/js/lib/**/*.js`).pipe(dest(`${destination}/js/lib`));
-  
+
   src(`${origin}/js/script.js`)
   .pipe(babel({
-    presets: ['@babel/env'] //to be compatible with all the browsers
-  }))
-
+    presets: ['@babel/env']
+  }))  
   .pipe(dest(`${destination}/js`));
   cb();
 }
 
 function watcher(cb) {
   watch(`${origin}/**/*.html`).on('change', series(html, browserSync.reload))
-  watch(`${origin}/**/*.css`).on('change', series(css, browserSync.reload))
+  watch(`${origin}/**/*.scss`).on('change', series(css, browserSync.reload))
   watch(`${origin}/**/*.js`).on('change', series(js, browserSync.reload))
   cb();
 }
