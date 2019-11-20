0415bee1ca47bad3c06a96aa0e3919b3ca0aca55
Joseph Safari <sajo2460hotmail.com>
2019-11-20 20:01:56 +0000

    Add a function server with Browsersync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,44 @@
-const {src, dest, series,parallel} = require('gulp');
+const {src, dest, series, parallel} = require('gulp');
 const del = require('del');
+const browserSync = require('browser-sync').create();
+
 const origin = 'src';
 const destination = 'build';
-
 
 async function clean(cb) {
   await del(destination);
   cb();
 }
-//defaultTask as html
+
 function html(cb) {
-    // place code for your default task here
-    /* src('src/index.html').pipe(dest('build'));
-     */
-  src(`${origin}**/*.html`).pipe(dest(destination))
+  src(`${origin}/**/*.html`).pipe(dest(destination));
   cb();
 }
+
 function css(cb) {
-  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`))
+  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`));
   cb();
 }
+
 function js(cb) {
   src([
     `${origin}/js/lib/bootstrap.bundle.min.js`,
     `${origin}/js/lib/fontawesome-all.min.js`,
     `${origin}/js/lib/jquery.min.js`,
     `${origin}/js/script.js`
-  ]).pipe(dest(`${destination}/js`))
+  ]).pipe(dest(`${destination}/js`));
   cb();
 }
 
-exports.default = series(clean, parallel( html, css, js));
+function server(cb) {
+  browserSync.init({
+    notify: false,
+    open: true,
+    server: {
+      baseDir: destination
+    }   
+  })
+  cb();
+}
+
+exports.default = series(clean, parallel(html, css, js), server);
